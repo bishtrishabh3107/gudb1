ed3668af4fa83b6bc78bfa819f123c12e415779b
bishtrishabh3107 <rishabhbisht@mac.local>
2021-11-17 21:09:15 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f0135f7ceb35dc1c02f417ad93edbc98'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4fa686f9-128b-48bf-b8fd-0dbf97deab5b'
+};
